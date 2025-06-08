c504f956459b2d32710a1f94bd61f9f7dac8787d
Danilo Soares <danilocs.network@gmail.com>
2025-06-07 22:45:36 -0300

    Cursos de COBOL da Alura

---
+++ 2 - Praticando Cobol - Variáveis Estruturais/ESQUELETO.cob
@@ -0,0 +1,23 @@
+      *----------------------------------------------------------------*
+       IDENTIFICATION DIVISION.
+      *-----------------------------------------------------------------
+       PROGRAM-ID. ESQUELETO.
+      *-----------------------------------------------------------------
+      * AUTHOR      DANILO SOARES (F2262206)
+      * OBJETIVO    HELLO WORLD
+      * DATA        --/01/2024
+      *-----------------------------------------------------------------
+       ENVIRONMENT DIVISION.
+      *-----------------------------------------------------------------
+       CONFIGURATION  SECTION.
+       SPECIAL-NAMES.
+           DECIMAL-POINT IS COMMA.
+      *-----------------------------------------------------------------
+       DATA DIVISION.
+       WORKING-STORAGE SECTION.
+      *-----------------------------------------------------------------
+       PROCEDURE DIVISION.
+       MAIN-PROCEDURE.
+           DISPLAY 'HELLO WORLD'.
+           STOP RUN.
+       END PROGRAM ESQUELETO.
