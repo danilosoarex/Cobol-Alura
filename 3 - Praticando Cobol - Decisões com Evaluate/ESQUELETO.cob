4c70dfdf299c9f1f65215a9d0f64f0857a3c31e3
Danilo Soares <danilocs.network@gmail.com>
2025-06-15 23:01:05 -0300

    Códigos dos cursos de Formação Cobol da Alura

---
+++ 3 - Praticando Cobol - Decisões com Evaluate/ESQUELETO.cob
@@ -0,0 +1,19 @@
+      *----------------------------------------------------------------*
+       IDENTIFICATION DIVISION.
+      *-----------------------------------------------------------------
+       PROGRAM-ID. ESQUELETO.
+      *-----------------------------------------------------------------
+       ENVIRONMENT DIVISION.
+      *-----------------------------------------------------------------
+       CONFIGURATION  SECTION.
+       SPECIAL-NAMES.
+           DECIMAL-POINT IS COMMA.
+      *-----------------------------------------------------------------
+       DATA DIVISION.
+       WORKING-STORAGE SECTION.
+      *-----------------------------------------------------------------
+       PROCEDURE DIVISION.
+       MAIN-PROCEDURE.
+           DISPLAY 'HELLO WORLD'.
+           STOP RUN.
+       END PROGRAM ESQUELETO.
